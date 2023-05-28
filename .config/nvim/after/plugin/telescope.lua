78223008c45f3046108d85d4bceb0ad3f1a11ba6
Kai Walter <kai.walter@outlook.de>
2023-05-28 15:48:36 +0200

    add NvimTree

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,4 +3,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find Files GIT" })
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end, { desc = "Find String with Grep" })
+end, { desc = "Find String with Grep (ripgrep must be installed)" })
--- .config/nvim/lua/kws/remap.lua
+++ .config/nvim/lua/kws/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- for NevimTree : disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
