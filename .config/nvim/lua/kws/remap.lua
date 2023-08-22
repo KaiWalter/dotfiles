2918f058a69e609b0b2a48376cc04929475189b7
Kai Walter <kai.walter@outlook.de>
2023-08-22 08:19:18 +0200

    x

--- .config/nvim/lua/kws/remap.lua
+++ .config/nvim/lua/kws/remap.lua
@@ -4,7 +4,7 @@ local keymap = vim.api.nvim_set_keymap
 local options = {noremap=true, silent=true}
 keymap("v","<leader>c", '"+y', options)
 keymap("n","<leader>v", '"+p', options)
-keymap("n","<leader>u", "<ESC>:PackerSync<CR>", {noremap=true, silent=true})
+keymap("n","<leader>u", "<ESC>:PackerSync<CR>", options)
 
 -- for NevimTree : disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
