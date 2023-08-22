e4d6076e5aeab1b5c55bce226cdab79a8b8622ad
Kai Walter <kai.walter@outlook.de>
2023-08-22 06:18:01 +0000

    add nvim line number

--- .config/nvim/lua/kws/remap.lua
+++ .config/nvim/lua/kws/remap.lua
@@ -1,10 +1,13 @@
 vim.g.mapleader = " "
+vim.wo.relativenumber = true
+vim.wo.number = true
 
 local keymap = vim.api.nvim_set_keymap
 local options = {noremap=true, silent=true}
 keymap("v","<leader>c", '"+y', options)
 keymap("n","<leader>v", '"+p', options)
-keymap("n","<leader>u", "<ESC>:PackerSync<CR>", {noremap=true, silent=true})
+keymap("n","<leader>u", "<ESC>:PackerSync<CR>", options)
+keymap("n","<leader>f", "<ESC>:LspZeroFormat<CR>", options)
 
 -- for NevimTree : disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
@@ -12,3 +15,4 @@ vim.g.loaded_netrwPlugin = 1
 
 local ft = require('Comment.ft')
 ft({'bicep'}, '//%s')
+
