6b3825d73d90065d9cde65d7e0422f6299bc271a
Kai Walter <kai.walter@outlook.de>
2023-09-09 17:23:46 +0200

    add azure functions module

--- .config/nvim/lua/kws/init.lua
+++ .config/nvim/lua/kws/init.lua
@@ -5,25 +5,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- install Packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
-end
-
 require('kws.utils')
 require('kws.packer')
-
-if is_bootstrap then
-  require('packer').sync()
-  print '==========================='
-  print 'Plugins are being installed'
-  print '==========================='
-  return
-end
-
 require('kws.remap')
--- require('kws.autosave')
--- .config/nvim/lua/kws/remap.lua
+++ .config/nvim/lua/kws/remap.lua
@@ -3,3 +3,4 @@ vim.g.maplocalleader = " "
 
 mapv("<leader>y", '"+y', "[Y]ank to system clipboard")
 mapn("<leader>p", '"+p', "[P]aste from system clipboard")
+mapn("<leader>h", ':nohlsearch<CR>', "clear search [H]ighlighting")
