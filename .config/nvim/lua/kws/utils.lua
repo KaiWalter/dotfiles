34ef48ce214393bf109ddd8c5ab8910e206719a9
Kai Walter <kai.walter@outlook.de>
2023-09-23 07:34:27 +0200

    use jose LSP

--- .config/nvim/lua/kws/utils.lua
+++ .config/nvim/lua/kws/utils.lua
@@ -1,18 +1,21 @@
-function map(mode, lhs, rhs, desc)
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = " "
-  local options = { noremap = true, silent = true, desc = desc }
-  vim.keymap.set(mode, lhs, rhs, options)
+function map(mode, lhs, rhs, desc, bufnr)
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
+	local options = { noremap = true, silent = true, desc = desc }
+	if bufnr then
+		options.bufnr = bufnr
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
 end
 
-function mapn(lhs, rhs, desc)
-  map('n', lhs, rhs, desc)
+function mapn(lhs, rhs, desc, bufnr)
+	map("n", lhs, rhs, desc, bufnr)
 end
 
-function mapv(lhs, rhs, desc)
-  map('v', lhs, rhs, desc)
+function mapv(lhs, rhs, desc, bufnr)
+	map("v", lhs, rhs, desc, bufnr)
 end
 
-function mapi(lhs, rhs, desc)
-  map('i', lhs, rhs, desc)
+function mapi(lhs, rhs, desc, bufnr)
+	map("i", lhs, rhs, desc, bufnr)
 end
