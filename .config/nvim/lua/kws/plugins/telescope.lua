dac6402b87cad3b5e104c3e915347f3fad87db4b
Kai Walter <kai.walter@outlook.de>
2023-09-24 20:42:24 +0200

    fzf win/linux

--- .config/nvim/lua/kws/plugins/telescope.lua
+++ .config/nvim/lua/kws/plugins/telescope.lua
@@ -1,35 +1,42 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = function()
+				return IsWindows()
+						and "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+					or "make"
+			end,
+		},
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,     -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-    MapN('<leader>ff', builtin.find_files, "[F]ind [F]iles")
-    MapN('<leader>fh', builtin.oldfiles, "[F]ile [H]istory")
-    MapN('<leader>fg', builtin.git_files, "[F]ind Files [G]IT")
-    MapN('<leader>fs', builtin.live_grep, "[F]ind [S]tring")
-    MapN('<leader>fc', builtin.grep_string, "[F]ind string under [C]ursor")
-  end,
+		MapN("<leader>ff", builtin.find_files, "[F]ind [F]iles")
+		MapN("<leader>fh", builtin.oldfiles, "[F]ile [H]istory")
+		MapN("<leader>fg", builtin.git_files, "[F]ind Files [G]IT")
+		MapN("<leader>fs", builtin.live_grep, "[F]ind [S]tring")
+		MapN("<leader>fc", builtin.grep_string, "[F]ind string under [C]ursor")
+	end,
 }
