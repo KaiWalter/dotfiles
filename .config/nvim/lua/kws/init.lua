da25b2c9596af9ddeb4f2fbc0ec66f2de4a49376
Kai Walter <kai.walter@outlook.de>
2023-10-01 16:57:26 +0200

    sync with josean

--- .config/nvim/lua/kws/init.lua
+++ .config/nvim/lua/kws/init.lua
@@ -2,17 +2,45 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.autoindent = true
-vim.o.smartindent = true
+local opt = vim.opt -- for conciseness
 
-vim.wo.relativenumber = true
-vim.wo.number = true
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
-vim.opt.clipboard = "unnamedplus"
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
 
 require("kws.utils")
 require("kws.lazy")
--- .config/nvim/lua/kws/remap.lua
+++ .config/nvim/lua/kws/remap.lua
@@ -11,8 +11,22 @@
 -- o = object under cursor (Treesitter)
 -- r = Rest
 -- s = Style / Formatting
+-- t = Tab operations
+-- w = Window/Split operations
 -- x = general operations
-MapN("<leader>bh", ":nohlsearch<CR>", "clear search [B]uffer [H]ighlighting")
+MapN("<leader>bh", "<cmd>nohlsearch<CR>", "clear search [B]uffer [H]ighlighting")
 MapN("<leader><leader>b", "<cmd>Telescope buffers<CR>", "[B]uffers")
 MapN("<leader>bn", "<cmd>bnext<CR>", "[B]uffer [N]ext")
 MapN("<leader>bp", "<cmd>bprev<CR>", "[B]uffer [P]revious")
+
+MapN("<leader>to", "<cmd>tabnew<CR>", "Open new tab") -- open new tab
+MapN("<leader>tx", "<cmd>tabclose<CR>", "Close current tab") -- close current tab
+MapN("<leader>tn", "<cmd>tabn<CR>", "Go to next tab") --  go to next tab
+MapN("<leader>tp", "<cmd>tabp<CR>", "Go to previous tab") --  go to previous tab
+MapN("<leader>tf", "<cmd>tabnew %<CR>", "Open current buffer in new tab") --  move current buffer to new tab
+
+-- window management
+MapN("<leader>wv", "<C-w>v", "Split window vertically") -- split window vertically
+MapN("<leader>wh", "<C-w>s", "Split window horizontally") -- split window horizontally
+MapN("<leader>we", "<C-w>=", "Make splits equal size") -- make split windows equal width & height
+MapN("<leader>wx", "<cmd>close<CR>", "Close current split") -- close current split window
