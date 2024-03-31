ec12616e7fe6b9689b5cf5700caea65dd5d64c05
Kai Walter <kai.walter@outlook.de>
2024-03-31 15:17:47 +0000

    tune projects telescope

--- .config/nvim/lua/kws/utils.lua
+++ .config/nvim/lua/kws/utils.lua
@@ -51,3 +51,14 @@ end
 function FolderExists(name)
 	return vim.fn.isdirectory(name) == 1
 end
+
+local function get_git_root()
+	local dot_git_path = vim.fn.finddir(".git", ".;")
+	return vim.fn.fnamemodify(dot_git_path, ":h")
+end
+
+vim.api.nvim_create_user_command("CdGitRoot", function()
+	vim.api.nvim_set_current_dir(get_git_root())
+end, {})
+
+MapN("<leader>xr", ":CdGitRoot<CR>", "cd to GIT [r]oot")
