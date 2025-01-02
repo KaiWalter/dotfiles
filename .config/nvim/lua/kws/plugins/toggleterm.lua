d8b3e44926306035833712f37fe9a4889d30dadb
Kai Walter <kai.walter@outlook.de>
2025-01-02 15:07:25 +0100

    reconfig toggleterm

--- .config/nvim/lua/kws/plugins/toggleterm.lua
+++ .config/nvim/lua/kws/plugins/toggleterm.lua
@@ -1,13 +1,13 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			open_mapping = [[<c-#>]],
-		})
-		MapV("<leader>s", function()
-			local trim_spaces = true
-			require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
-		end, "send to termimal")
-	end,
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-t>]],
+    })
+    MapV("<leader>s", function()
+      local trim_spaces = true
+      require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
+    end, "send to termimal")
+  end,
 }
