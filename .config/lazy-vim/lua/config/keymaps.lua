22229920de9928e19f751014a699885149d3fa2a
Kai Walter <kai.walter@outlook.de>
2025-02-16 18:57:59 +0100

    add own keymaps

--- .config/lazy-vim/lua/config/keymaps.lua
+++ .config/lazy-vim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+-- buffers
+map("n", "<C-PageUp>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<C-PageDown>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+map("n", "<M-j>", "<cmd>cprev<cr>", { desc = "Previous Quickfix" })
+map("n", "<M-k>", "<cmd>cnext<cr>", { desc = "Next Quickfix" })
