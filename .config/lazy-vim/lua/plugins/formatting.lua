0258e931fc4c1355305f8f73a886e74730b60cce
Kai Walter <kai.walter@outlook.de>
2025-06-01 08:54:53 +0200

    fix csharpier configuration

--- .config/lazy-vim/lua/plugins/formatting.lua
+++ .config/lazy-vim/lua/plugins/formatting.lua
@@ -1,7 +1,19 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters = {
+      csharpier = {
+        command = "csharpier",
+        args = { "format", "." }, -- ✅ required command and path
+        cwd = require("conform.util").root_file({
+          ".csharpier.json",
+          ".git",
+        }),
+        stdin = false,
+      },
+    },
     formatters_by_ft = {
+      cs = { "csharpier" },
       nix = { "alejandra" },
       bicep = { "bicep-lsp" },
     },
